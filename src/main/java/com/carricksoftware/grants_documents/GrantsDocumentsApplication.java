6a2073dfb82014cd59410da63f2dfb480b6518b5
Andrew Grant <carricksoft@gmail.com>
2025-04-19 15:07:28 +0100

    First Real commit

---
+++ src/main/java/com/carricksoftware/grants_documents/GrantsDocumentsApplication.java
@@ -0,0 +1,13 @@
+package com.carricksoftware.grants_documents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrantsDocumentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrantsDocumentsApplication.class, args);
+	}
+
+}
